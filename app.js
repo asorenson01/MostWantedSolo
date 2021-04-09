eb987c08be172f701279bf896191459ffa027609
unknown <asorenson01@hotmail.com>
2021-04-09 15:30:15 -0400

    grid built

--- app.js
+++ app.js
@@ -21,9 +21,36 @@ function searchByName(){
     }
 }
 
-function buildTheGrid()
-  
+
+function buildTheGrid(people){
+    let output = '';
+    let test = people.map(function(el){
+       return output +=
+        `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>
+        </tr>`  
+
+})
+document.getElementById("test").innerHTML = output
+}
+buildTheGrid(people)
+
+function sortByGender(){
+    let genderChoice = document.forms["genderForm"]["gender"]
+}
 
 
-buildTheGrid()
+
+
+
 
